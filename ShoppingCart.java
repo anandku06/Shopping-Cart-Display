50b896081f06f230f1a46788f63995110f833996
Anand Kumar <161126335+anandku06@users.noreply.github.com>
2025-01-05 20:21:52 +0530

    Add files via upload

---
+++ ShoppingCart.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ShoppingCart {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        String item;
+        double price;
+        int quantity;
+        char currency = '₹';
+        double total;
+
+        System.out.print("What items would you like to buy? ");
+        item = scanner.nextLine();
+
+        System.out.print("What is the price for each? ");
+        price = scanner.nextDouble();
+
+        System.out.print("How many would you like? ");
+        quantity = scanner.nextInt();
+
+        total = quantity * price;
+
+        System.out.println("\nYou have bought " + quantity + " " + item + "/s");
+        System.out.println("Your total is " + currency + total);
+
+        scanner.close();
+    }
+}
